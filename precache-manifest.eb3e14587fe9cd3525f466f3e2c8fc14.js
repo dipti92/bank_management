ca8841b08e18573fb2fac9891af9facb856a37be
dipti92 <babeldipti@gmail.com>
2020-10-18 14:37:16 +0530

    Updates

---
+++ precache-manifest.eb3e14587fe9cd3525f466f3e2c8fc14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8dbb2f3420e1f171a0afadbc0c727d7",
+    "url": "/bank_management/index.html"
+  },
+  {
+    "revision": "98d94c0437c16d1d6d1e",
+    "url": "/bank_management/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f7aae70f1c68d24df143",
+    "url": "/bank_management/static/css/main.5ed9b467.chunk.css"
+  },
+  {
+    "revision": "98d94c0437c16d1d6d1e",
+    "url": "/bank_management/static/js/2.ab6d7b86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bank_management/static/js/2.ab6d7b86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7aae70f1c68d24df143",
+    "url": "/bank_management/static/js/main.907200d0.chunk.js"
+  },
+  {
+    "revision": "0c55be2e2e732038e0e1",
+    "url": "/bank_management/static/js/runtime-main.758342be.js"
+  },
+  {
+    "revision": "b4bd873249ff70af22ae15cf01b139a0",
+    "url": "/bank_management/static/media/TDS-on-Bank-Deposit-and-Interest.b4bd8732.jpg"
+  }
+]);
